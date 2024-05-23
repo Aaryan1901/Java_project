1e989a4f351c0d48472a5a328eef5a609789f770
aaryan <aaryan@LAPTOP-0D1P8M16.mshome.net>
2024-05-23 15:26:18 +0530

    I am submitting java project

---
+++ Java_project/src/Assignment_1/DataType.java
@@ -0,0 +1,20 @@
+package Assignment_1;
+
+public class DataType {
+
+	public static void main(String[] args) {
+		float value2 = 101 / 61;
+		double value3 = 10d / 6d;
+	 //float division System.out.printin ("value 1=" + valuel);
+		System.out.println("value 2=" + value2);
+		System.out.println("value 3=" + value3);
+		int marker = 512;
+		//assigning expression to variable
+		double percentage = marker * 0.46f;
+		System.out.println("percentage :" + percentage);
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Java_project/src/Assignment_1/Identifier.java
@@ -0,0 +1,11 @@
+package Assignment_1;
+
+public class Identifier {
+
+	public static void main(String[] args) {
+		int $number =20;
+		System.out.println("the value of the number is:"+ $number);
+		String studentName="Aaryan";
+		System.out.println("the value is :"+ studentName);
+	}
+}
